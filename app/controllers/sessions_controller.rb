eba62a692a41c60dedf204e87f4ad67923ef9c30
Rishabh Gupta <rgupta@grepruby.com>
2017-09-18 16:41:55 +0530

    added logout and improved login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,15 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
-      redirect_to feed_path
+      redirect_to @user
     else
       flash.now[:alert] = 'invalid email/password combination'
       render :new
     end
   end
 
-  def destroy; end
+  def destroy
+    log_out
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'users#index'
   get 'users/index'
+  get '/users/:id', to: 'users#show', as: 'user'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  patch '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/feed', to: 'users#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
